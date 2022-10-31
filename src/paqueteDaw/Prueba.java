47fc864719ce7b64e57117b007432c34e0069bf9
ERICKBOWSER <guerig.eri@gmail.com>
2022-10-31 14:38:47 +0100

    subida utilidades

---
+++ src/paqueteDaw/Prueba.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paqueteDaw;
+
+/**
+ *
+ * @author guerig
+ */
+public class Prueba {
+    /*
+    MÉTODO MAIN PARA PROBAR LOS 4 MÉTODOS ANTERIORES
+    */
+}
